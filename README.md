dfce223ad3848a1775565d52d42bc018366f48d0
mitsumiza <sofiya-rolik-2006@mail.ru>
2025-10-01 14:20:50 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Characteristic Polynomial Calculator
+
+Python пакет для вычисления характеристических многочленов матриц графов с использованием SymPy и NumPy.
+
+## Установка
+
+```bash
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read().splitlines()
+
+setup(
+    name="characteristic-polynomial",
+    version="1.0.0",
+    author="Ваше Имя",
+    author_email="ваш.email@example.com",
+    description="Вычисление характеристических многочленов матриц графов",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+)
